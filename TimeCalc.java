208ced60df3226ea88cafa7095ad2877408cd99a
Ron Dor <reichmanrd@gmail.com>
2024-11-14 15:56:15 +0200

    Solved TimeCalc.java

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,21 @@
+// Notably, their way is the best way.
+// But, I'd like to try it differently.
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        // Receive input from client.
+        int DAILYMINUTES = 1440;
+        int DAILYHOURS = 24;
+        int MINUTESINHOUR = 60;
+        int orgHours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+        int orgMinutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int addMinutes = Integer.parseInt(args[1]);
+
+        // Rather than conversion to total minutes, let's look at the jumps in days.
+        int remainedMinutes = addMinutes % DAILYMINUTES;
+        int remainedHours = remainedMinutes / MINUTESINHOUR;
+        int newHours = (orgHours + remainedHours) % DAILYHOURS;
+        remainedMinutes = remainedMinutes - (remainedHours * MINUTESINHOUR);
+        int newMinutes = (orgMinutes + remainedMinutes) % 60;
+        System.out.println(newHours + ":" + newMinutes);
     }
 }
