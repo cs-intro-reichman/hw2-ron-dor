ff5cbd24f9b6d119dfbbef67ae49a081d4015a43
Ron Dor <reichmanrd@gmail.com>
2024-11-15 14:14:57 +0200

    Fixed CalcPi.java

--- CalcPi.java
+++ CalcPi.java
@@ -12,6 +12,6 @@ public class CalcPi {
 		sum *= 4;
 		// Print the results.
 		System.out.println("pi according to Java: " + Math.PI);
-		System.out.println("pi, approximated: " + sum);
+		System.out.println("pi, approximated:     " + sum);		
 	}
 }
